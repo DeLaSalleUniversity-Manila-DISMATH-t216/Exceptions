d4795f3f8b0895524c1ca19a636aa7d20488f056
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-16 19:05:17 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,18 @@
+package ph.edu.dlsu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            String[] str = {"Welcome!"};
+            System.out.println(str[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //e.printStackTrace();
+            System.out.println("There was an error");
+        }
+
+        System.out.println("The app is still running.");
+
+    }
+}
